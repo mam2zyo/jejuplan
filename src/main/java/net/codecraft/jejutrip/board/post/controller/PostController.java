4978939f7a011a47c0e13df497af80cdb20d9506
Park Shinhee <mam2zyo@gmail.com>
2025-07-25 18:04:43 +0900

    post controller api end point remaming

--- src/main/java/net/codecraft/jejutrip/board/post/controller/PostController.java
+++ src/main/java/net/codecraft/jejutrip/board/post/controller/PostController.java
@@ -17,12 +17,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/api/board")
 public class PostController {
 
     private final PostService postService;
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(value = "/post", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<ResponseMessage> addPost(@RequestPart PostRequest postRequest
             , @RequestPart(required = false) List<MultipartFile> multipartFiles , @CookieValue String accessToken)
             throws IOException {
@@ -31,27 +31,27 @@ public class PostController {
                 .body(postService.addPost(postRequest , multipartFiles , accessToken));
     }
 
-    @GetMapping(value = "/count")
+    @GetMapping(value = "/posts/count")
     public ResponseEntity<ResponseMessage> getTotalNumberOfPosts(@RequestParam String type , @RequestParam(required = false) String data) {
         return ResponseEntity.ok().body(postService.getTotalNumberOfPosts(type , data));
     }
 
-    @GetMapping
+    @GetMapping(value = "/posts")
     public ResponseEntity<ResponseMessage> getAllPost(Pageable pageable) {
         return ResponseEntity.ok().body(postService.getAllPost(pageable));
     }
 
-    @GetMapping(value = "/{postId}")
+    @GetMapping(value = "/posts/{postId}")
     public ResponseEntity<ResponseMessage> selectOnePost(@PathVariable("postId") Long postId) {
         return ResponseEntity.ok().body(postService.findPostByPostId(postId));
     }
 
-    @DeleteMapping(value = "/{postId}")
+    @DeleteMapping(value = "/posts/{postId}")
     public ResponseEntity<ResponseMessage> deletePost(@PathVariable("postId") Long postId , @CookieValue String accessToken) {
         return ResponseEntity.ok().body(postService.deletePost(postId , accessToken));
     }
 
-    @PutMapping(value = "/{postId}")
+    @PutMapping(value = "/posts/{postId}")
     public ResponseEntity<ResponseMessage> updatePost(@PathVariable("postId") Long postId
             , @CookieValue String accessToken , @RequestPart PostRequest postRequest
             , @RequestPart(required = false) List<MultipartFile> multipartFiles) throws IOException {
@@ -59,22 +59,22 @@ public class PostController {
         return ResponseEntity.ok().body(postService.updatePost(postId, accessToken, postRequest , multipartFiles));
     }
 
-    @PatchMapping(value = "/views/{postId}")
+    @PatchMapping(value = "/posts/views/{postId}")
     public ResponseEntity<ResponseMessage> increasePostViews(@PathVariable long postId) {
         return ResponseEntity.ok().body(postService.updatePostView(postId));
     }
 
-    @PatchMapping(value = "/likes/{postId}")
+    @PatchMapping(value = "/posts/likes/{postId}")
     public ResponseEntity<ResponseMessage> increasePostLike(@PathVariable Long postId , @CookieValue String accessToken) throws AccountException {
         return ResponseEntity.ok().body(postService.increasePostLike(postId, accessToken));
     }
 
-    @GetMapping(value = "/search/{postContent}")
+    @GetMapping(value = "/posts/search/{postContent}")
     public ResponseEntity<ResponseMessage> findPostBySearch(Pageable pageable, @PathVariable String postContent) {
         return ResponseEntity.ok().body(postService.findPostBySearch(pageable, postContent));
     }
 
-    @GetMapping(value = "/search/tags/{tagData}")
+    @GetMapping(value = "/posts/search/tags/{tagData}")
     public ResponseEntity<ResponseMessage> findPostByTag(Pageable pageable, @PathVariable String tagData) {
         return ResponseEntity.ok().body(postService.findPostBySearchAndTag(pageable, tagData));
     }
