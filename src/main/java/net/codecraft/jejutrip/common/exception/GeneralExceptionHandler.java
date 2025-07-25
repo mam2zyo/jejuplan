92ba4ca12db5fb16564478ca710758459bed851e
Park Shinhee <mam2zyo@gmail.com>
2025-07-26 00:16:11 +0900

    /common/exception/GeneralExceptionHandler.java 주석 제거

--- src/main/java/net/codecraft/jejutrip/common/exception/GeneralExceptionHandler.java
+++ src/main/java/net/codecraft/jejutrip/common/exception/GeneralExceptionHandler.java
@@ -3,10 +3,13 @@ package net.codecraft.jejutrip.common.exception;
 import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.http.HttpServletResponse;
 import net.codecraft.jejutrip.account.user.exception.LoginException;
+import net.codecraft.jejutrip.board.comment.exception.CommentException;
+import net.codecraft.jejutrip.board.post.exception.PostException;
 import net.codecraft.jejutrip.common.response.ResponseCode;
 import net.codecraft.jejutrip.common.response.ResponseMessage;
 import net.codecraft.jejutrip.security.jwt.exception.InvalidTokenException;
 import net.codecraft.jejutrip.security.jwt.support.CookieSupport;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -38,18 +41,18 @@ public class GeneralExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
     }
 
-//    @ExceptionHandler({PostException.class , CommentException.class})
-//    public ResponseEntity postExceptionHandler(Exception e) {
-//        ResponseMessage message = ResponseMessage.of(ResponseCode.REQUEST_FAIL , e.getMessage());
-//
-//        return ResponseEntity.badRequest().body(message);
-//    }
-//
-//    @ExceptionHandler({FileSizeLimitExceededException.class})
-//    public ResponseEntity fileUploadExceptionHandler(Exception e) {
-//        ResponseMessage message = ResponseMessage.of(ResponseCode.REQUEST_FAIL , e.getMessage());
-//
-//        return ResponseEntity.badRequest().body(message);
-//    }
+    @ExceptionHandler({PostException.class , CommentException.class})
+    public ResponseEntity postExceptionHandler(Exception e) {
+        ResponseMessage message = ResponseMessage.of(ResponseCode.REQUEST_FAIL , e.getMessage());
+
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler({FileSizeLimitExceededException.class})
+    public ResponseEntity fileUploadExceptionHandler(Exception e) {
+        ResponseMessage message = ResponseMessage.of(ResponseCode.REQUEST_FAIL , e.getMessage());
+
+        return ResponseEntity.badRequest().body(message);
+    }
 
 }
